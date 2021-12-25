959e68c56a2dee2ac57a6eaefdd618dbff903dbb
Simone Busoli <simone.busoli@gmail.com>
2021-12-25 19:31:53 +0100

    chore: prep for publishing

---
+++ index.ts
@@ -0,0 +1,4 @@
+export { default } from './core/pipeline'
+export * from './core/operations'
+export * from './core/operators'
+export * from './core/types'
---
+++ test-d/index.test-d.ts
@@ -0,0 +1,3 @@
+import pipeline from '../'
+
+new pipeline()
